2064ac7f4fcfb690162b02cbb8171eeac89df8d7
Ilyasa Adam N <ilyasagans@gmail.com>
2023-10-06 22:12:52 +0700

    lanjutkan update cart dan delete

--- app/src/main/java/com/example/challenge_4_ilyasa_adam_naufal/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/challenge_4_ilyasa_adam_naufal/Fragment/CartFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.challenge_4_ilyasa_adam_naufal.Adapter.CartAdapter
+import com.example.challenge_4_ilyasa_adam_naufal.DataClass.ItemMenu
 import com.example.challenge_4_ilyasa_adam_naufal.ViewModel.CartViewModel
 import com.example.challenge_4_ilyasa_adam_naufal.ViewModel.ViewModelFactory
 import com.example.challenge_4_ilyasa_adam_naufal.databinding.FragmentCartBinding
@@ -17,7 +18,7 @@ class CartFragment : Fragment() {
 	private lateinit var binding: FragmentCartBinding
 	private lateinit var cartViewModel: CartViewModel
 	private lateinit var cartAdapter: CartAdapter
-
+	private var item: ItemMenu? = null
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -43,4 +44,33 @@ class CartFragment : Fragment() {
 		val viewModelFactory = ViewModelFactory(requireActivity().application)
 		cartViewModel = ViewModelProvider(this, viewModelFactory)[CartViewModel::class.java]
 	}
+
+	/*private fun setData() {
+		@Suppress("DEPRECATION")
+		item = arguments?.getParcelable("item")
+
+		item?.let {
+			binding.imageadd.setImageResource(it.images)
+			binding.nameadd.text = item?.name
+			binding.priceadd.text = item?.price.toString()
+
+			viewModel.initSelectedItem(it)
+		}
+	}
+
+	private fun wViewModel() {
+		val observer = Observer<Int> {
+			binding.quantity.text = it.toString()
+		}
+
+		viewModel.counter.observe(viewLifecycleOwner, observer)
+
+		binding.btnadd1.setOnClickListener {
+			viewModel.increment()
+		}
+
+		binding.reduce.setOnClickListener {
+			viewModel.decrement()
+		}
+	}*/
 }
