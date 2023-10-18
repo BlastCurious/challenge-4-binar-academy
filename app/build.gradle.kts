37e4e634c2f232a6b35eadbcced5143a2c715789
Ilyasa Adam N <ilyasagans@gmail.com>
2023-10-18 16:47:13 +0700

    Buat halaman login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,23 +44,23 @@ android {
 
 dependencies {
 
-	implementation("androidx.core:core-ktx:1.9.0")
+	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.appcompat:appcompat:1.6.1")
-	implementation("com.google.android.material:material:1.9.0")
+	implementation("com.google.android.material:material:1.10.0")
 	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 	implementation("androidx.recyclerview:recyclerview:1.3.1")
 
 	//Navigation
-	implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-	implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
-	implementation("androidx.navigation:navigation-fragment:2.7.3")
+	implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+	implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+	implementation("androidx.navigation:navigation-fragment:2.7.4")
 
 	// ViewModel
-	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-	implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.5.1")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
 
 	//LiveData
-	implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+	implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 	implementation ("androidx.lifecycle:lifecycle-livedata-core-ktx:2.6.2")
 
 	//Database
@@ -68,6 +68,10 @@ dependencies {
 	implementation("androidx.room:room-ktx:2.5.2")
 	annotationProcessor("androidx.room:room-compiler:2.5.2")
 	kapt ("androidx.room:room-compiler:2.5.2")
+
+	//Glide
+	implementation ("com.github.bumptech.glide:glide:4.16.0")
+	annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
